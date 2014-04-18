7b77db5912f049d7d512884b8366eebab5a26585
jhorvat <julian.m.horvat@gmail.com>
2014-04-18 19:10:07 -0400

    Working on making backbone and require work

--- js/gcal-feed.js
+++ js/gcal-feed.js
@@ -1,50 +1,56 @@
-var clientID = '893172756640-ouu4h990t4vsq0mvuevv5grcs9bge18b.apps.googleusercontent.com'
-var apiKey = 'AIzaSyBwD1kBCk6ktJrmuogShXJlFM7ZQ4T-bs0'
-var scopes = 'https://www.googleapis.com/auth/calendar.readonly'
+(function ($, Backbone) {
+  var clientID = '893172756640-ouu4h990t4vsq0mvuevv5grcs9bge18b.apps.googleusercontent.com'
+  var apiKey = 'AIzaSyBwD1kBCk6ktJrmuogShXJlFM7ZQ4T-bs0'
+  var scopes = 'https://www.googleapis.com/auth/calendar.readonly'
 
-function appendResults (text) {
-  var results = document.getElementById('results')
-  results.appendChild(document.createElement('P'))
-  results.appendChild(document.createTextNode(text))
-}
-
-function makeRequest() {
-  gapi.client.load('calendar', 'v3', function() {
-    var request = gapi.client.calendar.events.list({
-      'calendarId': 'socis.ca_h8qcn7cgvfvpqgii9jkilb00r8@group.calendar.google.com',
-      'maxResults': 4,
-      'singleEvents': true
-    })
-    request.execute(function(resp) {
+  var Events = Backbone.Collection.extend({
+    url: function() {
+      return 'https://www.googleapis.com/calendar/v3/calendars/' + calID +'/events?maxResults=4&singleEvents=true&key=' + apiKey
+    },
+    parse: function(resp, xhr) {
       console.log(resp)
-    })
+      return resp.items
+    },
+    calID: 'socis.ca_h8qcn7cgvfvpqgii9jkilb00r8%40group.calendar.google.com'
   })
-}
 
-function handleAuthResult (authResult) {
-  console.log(authResult)
-  var authorizeButton = document.getElementById('auth')
-  var dashboard = document.getElementById('dash')
-  if (authResult && !authResult.error) {
-    dashboard.style.display = 'block'
-    authorizeButton.style.display = 'none'
-    console.log('authorized')
-    makeRequest()
-  } else {
-    console.log('couldn\'t auth')
-    gapi.auth.authorize({client_id: clientID, scope: scopes, immediate: false}, handleAuthResult);
-    dashboard.style.display = 'none'
-    authorizeButton.style.display = 'block'
-    authorizeButton.onclick = authClick
-  }
-}
+  EventView = Backbone.View.extend({
+    el: $("#events"),
+    initialize: function () {
+      gapi.client.setApiKey(apiKey)
+      gapi.auth.authorize({client_id: clientID, scope: scopes, immediate: true}, handleAuthResult);
+      this.eventCollection = new Events()
+    },
 
-function authClick (event) {
-  gapi.auth.authorize({client_id: clientID, scope: scopes, immediate: false}, handleAuthResult);
-  return false;
-}
+    render: function() {
+      var that = this
 
-function load() {
-  gapi.client.setApiKey(apiKey)
-  gapi.auth.authorize({client_id: clientID, scope: scopes, immediate: true}, handleAuthResult);
-}
+      this.eventCollection.fetch({
+        success: function(events) {
+          console.log(events)
+        }
+      })
+    },
+
+    handleAuthResult: function (authResult) {
+      console.log(authResult)
+      var authorizeButton = document.getElementById('auth')
+      var dashboard = document.getElementById('dash')
+      if (authResult && !authResult.error) {
+        dashboard.style.display = 'block'
+        authorizeButton.style.display = 'none'
+        console.log('authorized')
+      } else {
+        console.log('couldn\'t auth')
+        dashboard.style.display = 'none'
+        authorizeButton.style.display = 'block'
+        authorizeButton.onclick = authClick
+      }
+    },
+
+    authClick: function (event) {
+      gapi.auth.authorize({client_id: clientID, scope: scopes, immediate: false}, handleAuthResult);
+      return false;
+    }
+  })
+}) (jQuery);
